414b67d4cad7f123d760c060025b82e78753d0f9
David Pereira <d_pereira@outlook.com.br>
2018-07-20 20:56:57 -0300

    Alter Globalization to pt-BR, entire project

--- DevGeniusFinance/Global.asax.cs
+++ DevGeniusFinance/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevGeniusFinance.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,7 @@ namespace DevGeniusFinance
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
