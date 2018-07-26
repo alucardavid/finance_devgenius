85e6ac1b923c3a22fed8432948320a96455a49aa
David Pereira <d_pereira@outlook.com.br>
2018-07-25 23:14:06 -0300

    Inclusao sidebar, e footer

--- DevGeniusFinance/Scripts/_layout/main.js
+++ DevGeniusFinance/Scripts/_layout/main.js
@@ -1,8 +1,17 @@
 ﻿
 $(document).ready(function () {
 
-    $("#sidebarCollapse").click(function () {
-        $("#sidebar").toggleClass('active');
+    $('#sidebarCollapse').click(function () {
+        $('#sidebar').toggleClass('active');
     });
-    
+
+    $('body').on('click touchstart', function (e) {
+        if (!$(e.target).closest('#sidebar').length) {
+            if (!$(e.target).hasClass('oi oi-menu')) {
+                $('#sidebar').removeClass('active');
+            }
+        }
+    });
+
+
 });
